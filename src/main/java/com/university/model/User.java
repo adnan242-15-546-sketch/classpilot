62a854c81f810547c84131374e53b9da78c532c0
AdnanTheNerd <adnan242-15-546@diu.edu.bd>
2025-11-28 03:04:29 +0600

    Refactor comments in User.java for better clarity
    Updated comments in User class to improve clarity and maintainability.

--- src/main/java/com/university/model/User.java
+++ src/main/java/com/university/model/User.java
@@ -3,19 +3,19 @@ import java.io.Serializable;
 
 public abstract class User implements Serializable {
 
-    // Encapsulation: ডেটাগুলো private রাখা
+
     private String userId;
     private String password;
     private String fullName;
 
-    // Constructor: User অবজেক্ট তৈরির সময় এই ডেটাগুলো লাগবেই
+    
     public User(String userId, String password, String fullName) {
         this.userId = userId;
         this.password = password;
         this.fullName = fullName;
     }
 
-    // Getters: private ডেটাগুলো বাইরে থেকে দেখার জন্য
+    
     public String getUserId() {
         return userId;
     }
@@ -24,11 +24,11 @@ public abstract class User implements Serializable {
         return fullName;
     }
 
-    // --- মূল লজিক ---
+    
 
-    // পাসওয়ার্ড চেক করার মেথড
+    
     public boolean validatePassword(String inputPassword) {
-        // সহজ পাসওয়ার্ড চেকিং
+        
         return this.password.equals(inputPassword);
     }
 
